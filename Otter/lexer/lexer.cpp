4da4984a5bf196b857bb4d9eeec211cfacc0b5e6
Pranav Teegavarapu <pranavnt@outlook.com>
2020-11-24 21:32:17 -0800

    basic lexer done :D

--- Otter/lexer/lexer.cpp
+++ Otter/lexer/lexer.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 static std::string identifierStr;
-static double NumVal;
+static double numVal;
 
 // function for lexing
 static int getTokens()
@@ -30,7 +30,46 @@ static int getTokens()
         return tok_identifier;
     }
 
-    return -1;
+    // if char is a digit or . (decimal)
+    if (isdigit(lastChar) || lastChar == '.')
+    {
+        // string for numbers
+        std::string numStr;
+
+        // goes through string until it gets the full number
+        do
+        {
+            numStr += lastChar;
+            lastChar = getchar();
+        } while (isdigit(lastChar) || lastChar == '.');
+
+        // converts string to double
+        numVal = strtod(numStr.c_str(), 0);
+        return tok_number;
+    }
+
+    // checks for comments
+    // TODO: Switch to // and /*
+    if (lastChar == '#')
+    {
+        // ignores line if it starts with # (soon //)
+        do
+            lastChar = getchar();
+        while (lastChar != EOF && lastChar != '\n' && lastChar != '\r');
+
+        // making sure final char is not at EOF
+        if (lastChar != EOF)
+            return getTokens();
+    }
+
+    // checks if final character is eof
+    if (lastChar == EOF)
+        return tok_eof;
+
+    // returns final char
+    int thisChar = lastChar;
+    lastChar = getchar();
+    return thisChar;
 }
 
 int main()
